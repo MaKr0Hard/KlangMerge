4611fb1b10100dde81c243bd6179e4ed4a6820fd
MaKr0Hard <macrohard27@gmail.com>
2026-02-26 23:18:12 +0100

    Add files via upload
    no more aplay dependency

--- src/klangmerge.c
+++ src/klangmerge.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <SDL2/SDL.h>
+
 
 int main(int argc,char *argv[]) {
 if (argc <= 1) {
@@ -10,11 +12,35 @@ if (argc <= 1) {
 
 int is_it_emerge = system("which emerge > /dev/null 2>&1");
 
-int is_aplay_installed = system("which aplay > /dev/null 2>&1");
+// old int is_aplay_installed = system("which aplay > /dev/null 2>&1");
 
 
 if (is_it_emerge == 0) {
-	if (is_aplay_installed == 0) {
+	if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+		printf("SDL could not initialize : Press any key to start emerge or Ctrl + C to abort.\n");
+		getchar();
+		}
+		
+	SDL_AudioSpec wavSpec; //Loads file at the beginning so you will be warned for sure
+	Uint8* wavBuffer;
+	Uint32 wavLength;
+	if (SDL_LoadWAV("metal-pipe-clang.wav", &wavSpec, &wavBuffer, &wavLength) == NULL) {
+		printf("Error while loading sound file : %s\n", SDL_GetError());
+		SDL_FreeWAV(wavBuffer);
+		SDL_Quit();
+		return 1;
+	}
+	
+	SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+	if (deviceId == 0) {
+	printf("Error while opening audio device : %s\n", SDL_GetError());
+	SDL_FreeWAV(wavBuffer);
+	SDL_Quit();
+	return 1;
+	}
+	
+	
+	/* old   if (is_aplay_installed == 0) {*/
 	size_t total_length = 0;
 	for (int i = 1; i < argc; i++) {
 		total_length += strlen(argv[i]) + 1; 
@@ -37,11 +63,21 @@ if (is_it_emerge == 0) {
 	
 	system(result);
 	free(result);
+	
+	SDL_QueueAudio(deviceId, wavBuffer, wavLength);
+	SDL_PauseAudioDevice(deviceId, 0);
+	
+	SDL_Delay(3000); //adjust to your file's size (in milliseconds)
+	
+	
+	SDL_CloseAudioDevice(deviceId);
+	SDL_FreeWAV(wavBuffer);
+	SDL_Quit();
     	
-	system("aplay metal-pipe-clang.wav");
-	} else {
+	// old             system("aplay metal-pipe-clang.wav");
+	/* old   } else {
 	printf("Sorry, please install aplay.\n");
-	}
+	}*/
 	} else {
 	printf("You don't have emerge. Use Gentoo Linux, it's so easy to set up.\n");
 	}
